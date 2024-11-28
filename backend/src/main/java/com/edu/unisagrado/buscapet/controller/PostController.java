5052ba9a099e537e70f096746b0afb9ef2adfcac
Marcelo Augusto Coldibelli <marcelo.coldibelli@gmail.com>
2024-11-28 02:04:29 -0300

    refactor: changes endpoint to patch

--- backend/src/main/java/com/edu/unisagrado/buscapet/controller/PostController.java
+++ backend/src/main/java/com/edu/unisagrado/buscapet/controller/PostController.java
@@ -1,22 +1,24 @@
 package com.edu.unisagrado.buscapet.controller;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.ReflectionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -114,21 +116,22 @@ public class PostController {
         return ResponseEntity.ok(response);
     }
 
-	@PutMapping
+	@PatchMapping("/{idPost}")
 	@Transactional // Executa todas as ações (update) em conjunto e atualizar todas as colunas
-	public ResponseEntity updatePost(@RequestBody @Valid PostRequestDTO data) {
-		Optional<Post> optionalPost = postRepository.findById(data.getIdPost());
+	public ResponseEntity updatePost(@PathVariable Long idPost, @RequestBody @Valid PostRequestDTO data) {
+		Optional<Post> optionalPost = postRepository.findById(idPost);
 		if (optionalPost.isPresent()) {
 			Post postUpdate = optionalPost.get();
-			postUpdate.setPetName(data.getPetName());
-			postUpdate.setStatus(data.getStatus());
-			postUpdate.setImageName(data.getImageName());
-			postUpdate.setSpecies(data.getSpecies());
-			postUpdate.setDescription(data.getDescription());
-			postUpdate.setCity(data.getCity());
-			postUpdate.setNeighborhood(data.getNeighborhood());
-			postUpdate.setState(data.getState());
-			
+
+			if (data.getPetName() != null) postUpdate.setPetName(data.getPetName());
+			if (data.getStatus() != null) postUpdate.setStatus(data.getStatus());
+			if (data.getImageName() != null) postUpdate.setImageName(data.getImageName());
+			if (data.getSpecies() != null) postUpdate.setSpecies(data.getSpecies());
+			if (data.getDescription() != null) postUpdate.setDescription(data.getDescription());
+			if (data.getCity() != null) postUpdate.setCity(data.getCity());
+			if (data.getNeighborhood() != null) postUpdate.setNeighborhood(data.getNeighborhood());
+			if (data.getState() != null) postUpdate.setState(data.getState());
+
 			PostResponseDTO responseDTO = new PostResponseDTO(postUpdate);
 			return ResponseEntity.ok(responseDTO);
 		} else {
