81e31648f6d5da516f99a398f735eca87c187b25
Marcelo Augusto Coldibelli <marcelo.coldibelli@gmail.com>
2024-11-28 02:03:37 -0300

    feat: implements edit post

--- frontend/src/components/cards/LostFound.style.ts
+++ frontend/src/components/cards/LostFound.style.ts
@@ -17,7 +17,7 @@ export const CardContainer = styled.section`
         font-weight: bold;
         text-align: center;
         border-radius: 0.5rem 0.5rem 0 0;
-        padding: 1.25rem 0;
+        padding: 0.5rem 0;
     }
 
     .image-container {
@@ -39,37 +39,60 @@ export const CardContent = styled.div`
     display: flex;
     flex-direction: column;
     gap: 1.5rem;
-    padding: 1.5rem;
+    padding: 1rem;
 
     div {
         display: flex;
         justify-content: space-between;
         align-items: center;
-        font-size: 1.5rem;
     }
 
     h3 {
+        font-size: 1.5rem;
         font-weight: 600;
-        color: #424242;
+        color: #333333;
     }
 
     svg {
-        color: ${theme.colors.secondary}
+        font-size: 1.5rem;
+        color: ${theme.colors.secondary};
+        cursor: pointer;
+
     }
 
     p {
-        font-size: 1rem;
-        color: #424242;
+        font-size: 1.125rem;
+        color: #333333;
     }
     
-    span {
+    > span {
         display: flex;
-        align-items: center;
-        gap: 1rem;
+        flex-direction: column;
+    }
 
-        :hover {
-            color: ${theme.colors.primary};
-            cursor: pointer;
-        }
+    span:hover {
+        color: ${theme.colors.primary};
+    }
+
+    form {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 0.5rem;
+    }
+
+    form input {
+        width: 4rem;
+    }
+
+    form button {
+        padding: 0.25rem;
+        border: 2px solid ${theme.colors.primary};
+        border-radius: 0.5rem;
+    }
+    
+    button:hover {
+        font-weight: 700;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.15);
+        transition: 0.2s;
     }
 `;
